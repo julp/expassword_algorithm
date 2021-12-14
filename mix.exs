d22ae7fed9dfa725a143f1944007f8f95c826024
julp <e2c2be7ed0f2f336@gmail.com>
2021-12-14 19:37:53 +0100

    Version 0.2.0: merged into :expassword

--- mix.exs
+++ mix.exs
@@ -4,13 +4,13 @@ defmodule ExpasswordAlgorithm.MixProject do
   def project do
     [
       app: :expassword_algorithm,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
       package: package(),
-      source_url: "https://github.com/julp/expassword",
+      source_url: "https://github.com/julp/expassword_algorithm",
     ]
   end
 
